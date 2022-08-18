08726fd6d9d2da860faeded5682a6d2c580e08de
Emmanuel Albisser <emmanuel.albisser@gmail.com>
2022-08-18 15:24:41 +0200

    Initial Commit

---
+++ LITReview/reviews/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from LITReview.LITReview import settings
+
+
+class Ticket(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    image = models.ImageField(null=True,
+                              blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class Reviews(models.Model):
+    # Main model of this module, store reviews from user
+    ticket = models.ForeignKey(to=Ticket,
+                               on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # TODO DEMANDE pourquoi 1024 ?
+        max_length=1024,
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192,
+                            blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE,
+                             related_name='following')
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL,
+                                      on_delete=models.CASCADE,
+                                      related_name='followed_by')
+
+# TODO WHAT HELL IS IT ?
+    class Meta:
+        pass
