0fc8b107ab54e2a9ff71e8455566a8b65e90930c
Emmanuel Albisser <emmanuel.albisser@gmail.com>
2022-09-14 15:31:23 +0200

    folders refactored

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.http import HttpResponseRedirect
 from . import forms
 from .models import UserFollows, User
 
